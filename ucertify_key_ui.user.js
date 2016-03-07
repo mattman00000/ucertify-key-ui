f23a8afa53d3785ace74d58b3cf30dbb7a550034
mattman00000 <mattman00000@gmail.com>
2016-03-07 13:20:04 -0800

    0.0.2

--- ucertify_key_ui.user.js
+++ ucertify_key_ui.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ucertify Practice Exam Keyboard UI
 // @namespace    https://github.com/mattman00000/ucertify-key-ui
-// @version      0.0.1
+// @version      0.0.2
 // @description  adds left/right key for next/previous
 // @author       mattman00000
 // @match        https://www.ucertify.me/*
@@ -11,8 +11,11 @@
 (function() {
 	'use strict';
 	// Your code here...
+	GM_log("adding event listener");
 	window.addEventListener("keydown",function(a)
 	{
+		GM_log(a);
+		
 		var keyMap = 
 		{
 			"Right" : "next",
