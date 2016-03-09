63310f8b50fa4327ee450f667f314da315d899a2
mattman00000 <mattman00000@gmail.com>
2016-03-08 20:43:41 -0800

    0.0.6
    the style update

--- ucertify_key_ui.user.js
+++ ucertify_key_ui.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ucertify Practice Exam Keyboard UI
 // @namespace    https://github.com/mattman00000/ucertify-key-ui
-// @version      0.0.5
+// @version      0.0.6
 // @description  adds left/right key for next/previous
 // @author       mattman00000
 // @match        https://www.ucertify.me/*
@@ -12,28 +12,28 @@
 	'use strict';
 	// Your code here...
 	document.body.insertAdjacentHTML("afterBegin","\
-	<div id=\"key-ui-hud\" style=\"position: fixed; left: 0px; bottom: 0px; opacity: 0.8;\" class=\"col-md-12 npl\">\
-					        												<div class=\"col-md-6 col-lg-6 col-sm-6\">\
-											<div class=\"mb-md\"><b>Help : </b><span class=\"key\">/</span></div>\
-											<div class=\"mb-md\"><b>Dashboard : </b><span class=\"key\">&lt;Esc&gt; then H</span></div>\
-											<div class=\"mb-md\"><b>Prior : </b><span class=\"key\">P</span></div>\
-											<div class=\"mb-md\"><b>Next : </b><span class=\"key\">N</span></div>			\
-											<div class=\"mb-md\"><b>Bookmark : </b><span class=\"key\">M then B</span></div>			\
-											<div class=\"mb-md\"><b>Confidence : </b><span class=\"key\">M then C</span></div>			\
-											<div class=\"mb-md\"><b>Notes : </b><span class=\"key\">M then N</span></div>\
-											<div class=\"mb-md\"><b>Print : </b><span class=\"key\">CTRL + P</span></div>						\
-											<!-- <div class=\"mb-md\"><b>Rating : </b><span class=\"key\">S then 0-5</span></div> -->\
+	<div id=\"key-ui-hud\" style=\" margin: 2px; padding: 2px; position: fixed; left: 0px; bottom: 0px; opacity: 0.8;\" class=\"col-md-12 npl\">\
+					        												<div style=\" margin: 2px; padding: 2px;\" class=\"col-md-6 col-lg-6 col-sm-6\">\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Help : </b><span class=\"key\">/</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Dashboard : </b><span class=\"key\">&lt;Esc&gt; then H</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Prior : </b><span class=\"key\">P</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Next : </b><span class=\"key\">N</span></div>			\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Bookmark : </b><span class=\"key\">M then B</span></div>			\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Confidence : </b><span class=\"key\">M then C</span></div>			\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Notes : </b><span class=\"key\">M then N</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Print : </b><span class=\"key\">CTRL + P</span></div>						\
+											<!-- <div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Rating : </b><span class=\"key\">S then 0-5</span></div> -->\
 										</div>\
-										<div class=\"col-md-6 col-lg-6 col-sm-6\">\
-											<div class=\"mb-md\"><b>Answer Options : </b><span class=\"key\">1-9 or A-H</span></div>\
-											<div class=\"mb-md\"><b>Increase Time : </b><span class=\"key\">T</span></div>\
-											<!-- <div class=\"mb-md\"><b>Bookmark : </b><span class=\"key\">M</span></div> -->\
-											<div class=\"mb-md\"><b>Exhibit : </b><span class=\"key\">X</span></div>\
-											<div class=\"mb-md\"><b>Learn/Test : </b><span class=\"key\">&lt;Space&gt;</span></div>\
-											<div class=\"mb-md\"><b>Pause Test : </b><span class=\"key\">W</span></div>\
-											<div class=\"mb-md\"><b>Items List : </b><span class=\"key\">R</span></div>\
-											<div class=\"mb-md\"><b>End Test/Close : </b><span class=\"key\">Z</span></div>			\
-											<!--<div class=\"mb-md\"><b>End Test/Close : </b><span class=\"key\">E</span></div>-->\
+										<div style=\" margin: 2px; padding: 2px;\" class=\"col-md-6 col-lg-6 col-sm-6\">\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Answer Options : </b><span class=\"key\">1-9 or A-H</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Increase Time : </b><span class=\"key\">T</span></div>\
+											<!-- <div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Bookmark : </b><span class=\"key\">M</span></div> -->\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Exhibit : </b><span class=\"key\">X</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Learn/Test : </b><span class=\"key\">&lt;Space&gt;</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Pause Test : </b><span class=\"key\">W</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>Items List : </b><span class=\"key\">R</span></div>\
+											<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>End Test/Close : </b><span class=\"key\">Z</span></div>			\
+											<!--<div style=\" margin: 2px; padding: 2px;\" class=\"mb-md\"><b>End Test/Close : </b><span class=\"key\">E</span></div>-->\
 											<!---->\
 										</div>\
 																	</div>\
